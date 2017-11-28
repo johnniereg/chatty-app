f57cc255f8d9c35779a63c09e236fdf0eb90e813
Johnnie Regalado <johnniereg@gmail.com>
2017-11-28 22:31:21 +0000

    Chat messages implemented.
    On enter, message is passed up to App and added to state. Then rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,54 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
-let appState = {
-  currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-};
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = appState;
+    this.state = {
+      currentUser: { name: "Bob" }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(content) {
+    console.log(content);
+    const newMessage = {
+      id: (this.state.messages.length + 1),
+      username: "Test",
+      content: content
+    };
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({
+
+      messages: messages
+    })
+    
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = { id: 3, username: "Michelle", content: "Hello there!" };
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({ messages: messages })
+    }, 3000);
   }
 
   render() {
@@ -32,7 +60,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={ this.state.messages } />
-        <ChatBar currentUser={ this.state.currentUser } />
+        <ChatBar handleSubmit={this.handleSubmit} currentUser={ this.state.currentUser } />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,14 +4,25 @@ class ChatBar extends Component {
 
   constructor(props) {
     super(props);
+    this.getMessageContent = this.getMessageContent.bind(this);
   }
+
+  getMessageContent(e) {
+    let content = e.target.value;
+    if (e.key === 'Enter'){
+      this.props.handleSubmit(content);
+      
+      console.log("From ChatBar: ", content);
+    }
+  }
+
 
   render() {
 
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={ this.props.currentUser.name }/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.getMessageContent} />
       </footer>
     );
   }
