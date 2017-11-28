1663466adccf5864b7f5d7a695fa6e637d0a4ce4
Johnnie Regalado <johnniereg@gmail.com>
2017-11-28 23:58:10 +0000

    Update name on blur, clear message on enter.
    Better functionality. Also a minor colour tweak made to the banner and chat bar. I don't like tomato.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -6,32 +6,36 @@ class ChatBar extends Component {
     super(props);
     this.getMessageContent = this.getMessageContent.bind(this);
     this.getNewUserName = this.getNewUserName.bind(this);
+    this.state = {
+      username: this.props.currentUser.name
+    }
   }
 
+  // Listens for 'enter' and submits, then clears field.
   getMessageContent(e) {
     let content = e.target.value;
     if (e.key === 'Enter'){
-      this.props.handleSubmit(content); 
+      this.props.handleSubmit(content);
+      e.target.value=''; 
     }
   }
 
+  // Update username when focus leaves username field.
   getNewUserName(e) {
     let name = e.target.value;
-    if (e.key === 'Enter'){
-      this.props.handleNameChange(name);
-    }
+    this.props.handleNameChange(name);
   }
-
 
   render() {
 
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={ this.props.currentUser.name } onKeyPress={ this.getNewUserName } />
+        <input className="chatbar-username" defaultValue={ this.state.username } onBlur={ this.getNewUserName } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={ this.getMessageContent } />
       </footer>
     );
   }
 }
+
 export default ChatBar;
 
