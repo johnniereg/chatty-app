fe81b31fe510111837a8b032d27c4b3c570ca474
Johnnie Regalado <johnniereg@gmail.com>
2017-11-28 20:15:33 +0000

    Unique message IDs.
    Each message in array is rendered and given unique key.

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,12 +2,27 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  
   render() {
+    const messages = this.props.messages.map((message) => {
+      return <Message
+        key={ message.id }
+        username={ message.username }
+        content={ message.content }
+        />
+    });
+
     console.log("Rendering <MessageList/>");
+
     return (
       <div>
         <main className="messages">
-          <Message />
+          { messages }
         </main>
       </div>
     );
